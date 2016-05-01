809b68e893f80ddc99dad5b2117f90d9876cea13
CTD2 <cityTD2@gmail.com>
2016-04-30 23:00:12 -0400

    Update canvasTesting.js

--- canvasTesting.js
+++ canvasTesting.js
@@ -69,9 +69,7 @@ canvas.on('mouse:out', function (e) {
     var isTowerObj = towerObjs.indexOf(e.target) > -1;
     var pointer = canvas.getPointer(e.e);
   	var posX = pointer.x;
-  	var posY = pointer.y;
-  	console.log(posX + ", " + posY);
-    if (!isRangeObj && e.target.filters.length > 0 && fabricPath.indexOf(e.target) < 0) {
+    if (posX >= 0 && !isRangeObj && e.target.filters.length > 0 && fabricPath.indexOf(e.target) < 0) {
         e.target.filters.splice(e.target.filters.indexOf(hoverFilter),1);
         e.target.applyFilters(canvas.renderAll.bind(canvas));
     }
