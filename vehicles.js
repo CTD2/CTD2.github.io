1e69d89028069886cda3ac899c5342de2306f7b7
CTD2 <cityTD2@gmail.com>
2016-06-06 13:40:27 -0400

    Nerfed money gained from vehicles

--- vehicles.js
+++ vehicles.js
@@ -7,29 +7,29 @@
 //Vehicles(hp, type, gains, loss, coordX, coordY, cityObject)
 function makeBike()
 {
-  return new Vehicles(30, "bike", 50, 1, Scoordx, Scoordy, PLAYER) ;
+  return new Vehicles(30, "bike", 10, 1, Scoordx, Scoordy, PLAYER) ;
 }
 function makeMotorcycle()
 {
-  return new Vehicles(100, "motorcycle", 75, 2, Scoordx, Scoordy, PLAYER) ;
+  return new Vehicles(100, "motorcycle", 20, 2, Scoordx, Scoordy, PLAYER) ;
 }
 function makeSmartCar()
 {
-  return new Vehicles(250, "smartCar", 125, 5, Scoordx, Scoordy, PLAYER) ;
+  return new Vehicles(250, "smartCar", 35, 5, Scoordx, Scoordy, PLAYER) ;
 }
 function makeSportsCar()
 {
-  return new Vehicles(600, "sportsCar", 200, 10, Scoordx, Scoordy, PLAYER) ;
+  return new Vehicles(600, "sportsCar", 55, 10, Scoordx, Scoordy, PLAYER) ;
 }
 function makeSUV()
 {
-  return new Vehicles(1500, "SUV", 275, 20, Scoordx, Scoordy, PLAYER) ;
+  return new Vehicles(1500, "SUV", 275, 80, Scoordx, Scoordy, PLAYER) ;
 }
 function makeTruck()
 {
-  return new Vehicles(3500, "truck", 375, 40, Scoordx, Scoordy, PLAYER) ;
+  return new Vehicles(3500, "truck", 375, 110, Scoordx, Scoordy, PLAYER) ;
 }
 function makeGarbageTruck()
 {
-  return new Vehicles(10000, "garbageTruck", 500, 50, Scoordx, Scoordy, PLAYER) ;
+  return new Vehicles(10000, "garbageTruck", 200, 50, Scoordx, Scoordy, PLAYER) ;
 }
